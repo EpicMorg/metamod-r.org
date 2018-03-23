e93f7cd95377a5b36ea6e44096c43fab83d2e840
STAM <stamepicmorg@users.noreply.github.com>
2018-03-23 18:32:54 +0300

    Update bulma.js

--- src/assets/js/bulma.js
+++ src/assets/js/bulma.js
@@ -7,11 +7,11 @@
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
     });
-    var dropdown = document.querySelector('.dropdown');
+ /*   var dropdown = document.querySelector('.dropdown');
 	dropdown.addEventListener('click', function(event) {
 	  event.stopPropagation();
 	  dropdown.classList.toggle('is-active');
-	});
+	});*/
     var hellopreloader = document.getElementById("hellopreloader_preload");
     function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
 })();
