366d0cdb67dd6d6a30d354e570644261cddd2503
STAM <stam@epicm.org>
2018-03-26 18:13:02 +0300

    pre-release-0.0.0.3
    - migrated to BS4 from BulmaIO

---
+++ src/assets/js/bootstrap.engine.dev.js
@@ -0,0 +1,35 @@
+////////////////////////////
+// Sorry for my bad code. //
+////////////////////////////
+
+$(document).ready(function(){
+//////////////////
+// Native js
+/////////////////
+
+// Loader init 
+    function fadeOutnojquery(el) {
+        el.style.opacity = 1;
+        var interhellopreloader = setInterval(function() {
+            el.style.opacity = el.style.opacity - 0.05;
+            if (el.style.opacity <= 0.05) {
+                clearInterval(interhellopreloader);
+                hellopreloader.style.display = "none";  
+            }
+        }, 16);
+    }
+    window.onload = function() { 
+        setTimeout(function() {
+            fadeOutnojquery(hellopreloader);
+        }, 1000);  
+    };
+
+//////////////////
+// jQuery js:
+//////////////////
+});
+
+
+     
+
+
