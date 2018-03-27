d219e1cc5c7adac171cc8dcc8baad1c89c9f93ef
STAM <stam@epicm.org>
2018-03-27 18:40:30 +0300

    easteregg
    * animate css
    * animate css js plugin
    * ui fixes

--- src/assets/js/bootstrap.engine.dev.js
+++ src/assets/js/bootstrap.engine.dev.js
@@ -27,9 +27,41 @@ $(document).ready(function(){
 //////////////////
 // jQuery js:
 //////////////////
+
+    $( "#metaheader" ).click(function() {
+        $('#metaheader').addClass('animated hinge');
+        $('#metaheader').animateCSS('hinge', 2000, function() {
+              // Animation complete!
+              $('#metaheader').addClass("fadeIn");
+              $('#metaheader').removeClass("hinge");
+              $('#metaheader').removeClass("animated");
+        });
+    }); 
+    $( "#whatisit" ).click(function() {
+        $('#whatisit').addClass('animated flip');
+        $('#whatisit').animateCSS('flip', 1000, function() {
+              // Animation complete!
+              $('#whatisit').removeClass("animated");
+              $('#whatisit').removeClass("flip");
+        });
+    }); 
+    $( "#clickme-trigger" ).click(function() {
+       $('.aenimka-the-unicorn').removeClass("hidden");
+       $('.aenimka-the-unicorn').addClass('animated zoomIn');
+       $('.aenimka-the-unicorn').animateCSS('zoomIn', 2000, function() {
+          // Animation complete!
+          $('.aenimka-the-unicorn').addClass('animated zoomOut');
+          $('.aenimka-the-unicorn').animateCSS('zoomOut', 1000, function() {
+              // Animation complete!
+              $('.aenimka-the-unicorn').addClass("hidden");
+              $('.aenimka-the-unicorn').removeClass("animated");
+              $('.aenimka-the-unicorn').removeClass("zoomOut");
+              $('.aenimka-the-unicorn').removeClass("zoomIn");
+              window.open('https://github.com/AEnimka', '_blank').focus();
+          });
+       });
+    });
+
+//////////////////
 });
-
-
-     
-
-
+ 
